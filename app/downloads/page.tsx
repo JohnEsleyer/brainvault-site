9b27dc2b91e1f9cb00b04c0faf12d9e8ae341fc0
john <ralphesleyer@gmail.com>
2023-09-16 23:16:51 +0800

    Added downloads page

---
+++ app/downloads/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function DownloadsPage(){
+    return (
+        <>
+        <h1> This is the downloads page</h1>
+        </>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Image from 'next/image'
+import {useRouter} from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  
+  const handleClick = () => {
+    router.push('/downloads');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="text-gray-400 bg-black body-font">
@@ -8,10 +16,14 @@ export default function Home() {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Store knowledge as snippets
             </h1>
-            <p className="mb-8 leading-relaxed">BrainVault is a markdown note-taking app, your dedicated personal knowledge hub. With a focus on helping you remember, it takes a unique approach. BrainVault reimagines notes as flashcards, not just documents. It's all about storing bite-sized information in a simpler way.</p>
+            <p className="mb-8 leading-relaxed">BrainVault is a markdown note-taking application, serving as your dedicated personal knowledge hub. With a primary emphasis on aiding memory retention, it offers a distinctive approach. BrainVault reimagines notes as flashcards rather than mere documents, making it all about storing bite-sized information in a more straightforward manner.</p>
             <div className="flex justify-center">
-              <button className="inline-flex text-black bg-slate-50 border-0 py-2 px-6 focus:outline-none rounded text">Download</button>
-              <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text">Source Code</button>
+              <button className="inline-flex text-black bg-slate-50 border-0 py-2 px-6 focus:outline-none rounded text" onClick={handleClick}>Download</button>
+              <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text">
+                <a href="https://github.com/JohnEsleyer/brainvault">
+                Source Code
+                </a>
+                </button>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
